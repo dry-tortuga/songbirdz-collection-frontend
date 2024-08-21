9287c124e63425d07719ebafcc8a22a2e9d46007
Dry Tortuga <108436943+dry-tortuga@users.noreply.github.com>
2024-08-21 19:25:59 -0400

    Enable basenames (#18)

--- src/components/AccountOwner.js
+++ src/components/AccountOwner.js
@@ -1,12 +1,29 @@
 import React from "react";
 import { Avatar, Name } from "@coinbase/onchainkit/identity";
+import { base, baseSepolia, hardhat } from "viem/chains";
+
+let chain = base;
+
+if (process.env.REACT_APP_NODE_ENV === "development") {
+
+	chain = hardhat;
+
+} else if (process.env.REACT_APP_NODE_ENV === "staging") {
+
+	chain = baseSepolia;
+
+}
 
 const AccountOwner = ({ account, size = "sm", className }) => (
 	<div className={`flex h-10 items-center space-x-4 ${className || ""}`}>
-		<Avatar	address={account} />
+		<Avatar
+			address={account}
+			chain={chain} />
 		<div className={`ms-1 flex flex-col ${size === "sm" ? "text-sm" : ""}`}>
 			<b>
-				<Name address={account} />
+				<Name
+					address={account}
+					chain={chain} />
 			</b>
 		</div>
 	</div>
