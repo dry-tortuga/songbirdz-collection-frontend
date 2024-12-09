b5dee7cf48fef9e5836e7499c4262d4f77a0f42a
drytortuga <108436943+dry-tortuga@users.noreply.github.com>
2024-12-09 10:33:58 -0500

    kick off season 3

--- src/hooks/useLeaderboard.js
+++ src/hooks/useLeaderboard.js
@@ -5,33 +5,47 @@ import { fetchLeaderboard } from "../utils/data";
 const TOTAL_SIZE = 52;
 
 const useLeaderboard = ({ account, season }) => {
-  const [data, setData] = useState(null);
 
-  // Reset the leaderboard data if the account changes
-  useEffect(() => setData(null), [account]);
+    const [data, setData] = useState(null);
 
-  // Fetch the leaderboard data
-  useEffect(() => {
-    if (!data && season >= 1) {
-      const fetch = async () => {
-        // Fetch the leaderboard data from the backend server
-        const users = await fetchLeaderboard(season, account, TOTAL_SIZE);
+    // Reset the leaderboard data if the account changes
+    useEffect(() => setData(null), [account]);
 
-        setData({
-          users,
-          timestampMessage:
-            season === 1
-              ? "Results are final as of August 31st, 2024 11PM UTC."
-              : "Results are final as of November 30th, 2024 11PM UTC.",
-          // "Leaderboard attempts to update in real-time, but points to be manually confirmed on a weekly basis in case any ERC-721 events are missed.",
-        });
-      };
+    // Fetch the leaderboard data
+    useEffect(() => {
 
-      fetch();
-    }
-  }, [data, account, season]);
+        if (!data && season >= 1) {
 
-  return { data, setData };
+            const fetch = async () => {
+
+                // Fetch the leaderboard data from the backend server
+                const users = await fetchLeaderboard(season, account, TOTAL_SIZE);
+
+                let timestampMessage;
+
+                if (season === 1) {
+                    timestampMessage = "Results are final as of August 31st, 2024 11PM UTC.";
+                } else if (season === 2) {
+                    timestampMessage = "Results are final as of November 30th, 2024 11PM UTC.";
+                } else if (season === 3) {
+                    timestampMessage = "Runs from December 1st, 2024 12AM UTC to February 28th, 2025 11PM UTC.";
+                }
+
+                setData({
+                    users,
+                    timestampMessage,
+                });
+
+            };
+
+            fetch();
+
+        }
+
+    }, [data, account, season]);
+
+    return { data, setData };
+
 };
 
 export default useLeaderboard;
