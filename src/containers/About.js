4c45d1109bb1d202beafdcc523924fdb6e6e2120
dry-tortuga <108436943+dry-tortuga@users.noreply.github.com>
2025-04-10 15:16:51 -0400

    add team section to the about page

--- src/containers/About.js
+++ src/containers/About.js
@@ -27,7 +27,7 @@ const About = () => {
                             {" chosen in the \"GAMING with thirdweb: Gaming\" category at the Onchain Summer Buildathon in June 2024."}
                         </p>
                         <p>
-                            {"There will be a total of 10 flocks, each consisting of 1,000 birds, which each flock rolled out one-at-a-time and with a distinct set of species, song audio, and images. Starting with the first flock of 1,000 (i.e. the Picasso Genesis flock), and ending once we've reached 10,000 total birds in the wild. The next flock of 1,000 birds won't be released until all birds in the previous flock have been successfully identified. The styling of future flocks could draw inspiration from Jackson Pollock, Cubism, Popart, Art Deco, etc., and may feature different species of birds, with input from the community."}
+                            {"There will be a total of 10 flocks, each consisting of 1,000 birds, which each flock rolled out one-at-a-time and with a distinct set of species, song audio, and images. Starting with the first flock of 1,000 (i.e. the Picasso Genesis flock), and ending once we've reached 10,000 total birds in the wild. The next flock of 1,000 birds won't be released until all birds in the previous flock have been successfully identified. Future flocks will introduce new species of birds, with input from the community."}
                         </p>
                         <p>
                             {"Curated by a life-long birder, using images from "}
@@ -49,7 +49,7 @@ const About = () => {
                             {"."}
                         </p>
                         <p>
-                            {"The contract uses merkle trees to store onchain hashes of the species names, images, and song audio for each flock in the collection. Once all 10,000 in the wild have been identified this data will be uploaded to a decentralized storage provider such as ipfs via nft.storage. The contract will then be updated to freeze this metadata, the back-end web server will be shut down in favor of a 2nd smart contract to manage the points system and life list, and there will be no more of a official roadmap for this project."}
+                            {"The contract uses merkle trees to store onchain hashes of the species names, images, and song audio for each flock in the collection. Once all 10,000 in the wild have been identified this data will be uploaded to ipfs. The back-end web server will be shut down in favor of a 2nd smart contract to manage the points system and life list. The long-term goal for this project is to be a community-driven ecosystem for bird enthusiasts, fostering a deeper connection with nature and promoting conservation efforts, while also providing a platform for users to engage with and learn about birds (and web3) in a fun and interactive way."}
                         </p>
                         <p>
                             {"The smart contract governing the NFTs, the back-end code for the web server, and the front-end code for the web application are all open-source (MIT License). The images (and species metadata) associated with each NFT in the collection is released under the Creative Commons Zero (CC0) license, granting users freedom to use, remix, and share the artworks without any restrictions. "}
@@ -57,92 +57,8 @@ const About = () => {
                             {" If you wish to use the audio mp3 files in your own project, you must purchase them for $20.00 from \"The Cornell Guide to Bird Sounds: United States and Canada (v2021)\". See link above."}
                         </p>
                         <p>
-                            {"As a new onchain developer and an avid appreciator of birds, I hope you find this project interesting, useful, and enjoyable. Thank you!"}
+                            {"As a new onchain developer and an avid appreciator of birds, I hope you find this project interesting, useful, and enjoyable. I invite you to join the community and contribute to the project. Thank you!"}
                         </p>
-                        <h2>{"Resources"}</h2>
-                        <ul style={{ listStyle: "disc" }}>
-                            <li>
-                                <a
-                                    href="https://twitter.com/songbirdz_cc"
-                                    target="_blank"
-                                    rel="noopener noreferrer nofollow">
-                                    <b>{"Twitter"}</b>
-                                </a>
-                            </li>
-                            <li>
-                                <a
-                                    href="https://t.me/songbirdz_cc"
-                                    target="_blank"
-                                    rel="noopener noreferrer nofollow">
-                                    <b>{"Telegram"}</b>
-                                </a>
-                            </li>
-                            <li>
-                                <a
-                                    href="https://x.com/BitHomepage/status/1844090067876548734"
-                                    target="_blank"
-                                    rel="noopener noreferrer nofollow">
-                                    <b>{"AMA #1 (10/09/2024)"}</b>
-                                </a>
-                            </li>
-                            <li>
-                                <Link  to="/sounds-of-summer-2024">
-                                    <b>{"Sounds of Summer (Free Mint)"}</b>
-                                </Link>
-                            </li>
-                            <li>
-                                <a
-                                    href="https://basescan.org/address/0x7C3B795e2174C5E0C4F7d563A2FB34F024C8390B"
-                                    target="_blank"
-                                    rel="noopener noreferrer nofollow"
-                                    style={{
-                                        wordBreak: "break-word",
-                                        wordWrap: "break-word",
-                                    }}>
-                                    <b>{"Deployed Contract on Base - 0x7C3B795e2174C5E0C4F7d563A2FB34F024C8390B"}</b>
-                                </a>
-                            </li>
-                            <li>
-                                <a
-                                    href="https://github.com/dry-tortuga/songbirdz-collection-backend/blob/main/contracts/SongBirdz.sol"
-                                    target="_blank"
-                                    rel="noopener noreferrer nofollow">
-                                    <b>{"Solidity Contract"}</b>
-                                </a>
-                            </li>
-                            <li>
-                                <a
-                                    href="https://github.com/dry-tortuga/songbirdz-collection-backend"
-                                    target="_blank"
-                                    rel="noopener noreferrer nofollow">
-                                    <b>{"Back-End Server"}</b>
-                                </a>
-                            </li>
-                            <li>
-                                <a
-                                    href="https://github.com/dry-tortuga/songbirdz-collection-frontend"
-                                    target="_blank"
-                                    rel="noopener noreferrer nofollow">
-                                    <b>{"Front-End Application"}</b>
-                                </a>
-                            </li>
-                            <li>
-                                <a
-                                    href="https://github.com/dry-tortuga/songbirdz-collection-media"
-                                    target="_blank"
-                                    rel="noopener noreferrer nofollow">
-                                    <b>{"Media & Metadata"}</b>
-                                </a>
-                            </li>
-                            <li>
-                                <a
-                                    href="https://www.redbubble.com/people/drytortuga/shop"
-                                    target="_blank"
-                                    rel="noopener noreferrer nofollow">
-                                    <b>{"Songbirdz Merch"}</b>
-                                </a>
-                            </li>
-                        </ul>
                         <h2>{"Flocks in the Wild"}</h2>
                         <ul style={{ listStyle: "disc" }}>
                             <li>
@@ -250,6 +166,159 @@ const About = () => {
                                 {" and 50% will go to the project creators."}
                             </li>
                         </ul>
+                        <h2>{"Resources"}</h2>
+                        <ul style={{ listStyle: "disc" }}>
+                            <li>
+                                <a
+                                    href="https://twitter.com/songbirdz_cc"
+                                    target="_blank"
+                                    rel="noopener noreferrer nofollow">
+                                    <b>{"Twitter"}</b>
+                                </a>
+                            </li>
+                            <li>
+                                <a
+                                    href="https://t.me/songbirdz_cc"
+                                    target="_blank"
+                                    rel="noopener noreferrer nofollow">
+                                    <b>{"Telegram"}</b>
+                                </a>
+                            </li>
+                            <li>
+                                <a
+                                    href="https://x.com/BitHomepage/status/1844090067876548734"
+                                    target="_blank"
+                                    rel="noopener noreferrer nofollow">
+                                    <b>{"AMA #1 (10/09/2024)"}</b>
+                                </a>
+                            </li>
+                            <li>
+                                <Link  to="/sounds-of-summer-2024">
+                                    <b>{"Sounds of Summer (Free Mint)"}</b>
+                                </Link>
+                            </li>
+                            <li>
+                                <a
+                                    href="https://basescan.org/address/0x7C3B795e2174C5E0C4F7d563A2FB34F024C8390B"
+                                    target="_blank"
+                                    rel="noopener noreferrer nofollow"
+                                    style={{
+                                        wordBreak: "break-word",
+                                        wordWrap: "break-word",
+                                    }}>
+                                    <b>{"Deployed Contract on Base - 0x7C3B795e2174C5E0C4F7d563A2FB34F024C8390B"}</b>
+                                </a>
+                            </li>
+                            <li>
+                                <a
+                                    href="https://github.com/dry-tortuga/songbirdz-collection-backend/blob/main/contracts/SongBirdz.sol"
+                                    target="_blank"
+                                    rel="noopener noreferrer nofollow">
+                                    <b>{"Solidity Contract"}</b>
+                                </a>
+                            </li>
+                            <li>
+                                <a
+                                    href="https://github.com/dry-tortuga/songbirdz-collection-backend"
+                                    target="_blank"
+                                    rel="noopener noreferrer nofollow">
+                                    <b>{"Back-End Server"}</b>
+                                </a>
+                            </li>
+                            <li>
+                                <a
+                                    href="https://github.com/dry-tortuga/songbirdz-collection-frontend"
+                                    target="_blank"
+                                    rel="noopener noreferrer nofollow">
+                                    <b>{"Front-End Application"}</b>
+                                </a>
+                            </li>
+                            <li>
+                                <a
+                                    href="https://github.com/dry-tortuga/songbirdz-collection-media"
+                                    target="_blank"
+                                    rel="noopener noreferrer nofollow">
+                                    <b>{"Media & Metadata"}</b>
+                                </a>
+                            </li>
+                            <li>
+                                <a
+                                    href="https://www.redbubble.com/people/drytortuga/shop"
+                                    target="_blank"
+                                    rel="noopener noreferrer nofollow">
+                                    <b>{"Songbirdz Merch"}</b>
+                                </a>
+                            </li>
+                        </ul>
+                        <h1 className="mb-3">{"The Aviary"}</h1>
+                        <div
+	                        style={{
+	                            display: "flex",
+	                            flexDirection: "row",
+	                            gap: "2rem"
+	                        }}>
+                            <div
+	                            style={{
+		                            display: "flex",
+		                            flexDirection: "column",
+		                            alignItems: "center",
+		                            marginBottom: "1.5rem"
+	                            }}>
+                                <img
+                                    src="https://songbirdz.cc/images/2.jpg"
+                                    className="rounded mb-2"
+                                    title="Red-winged Blackbird"
+                                    style={{ width: "150px", height: "150px", objectFit: "cover" }} />
+                                <h3 style={{ margin: "0.25rem 0" }}>
+                                    {"Dry Tortuga"}
+                                </h3>
+                                <p style={{ margin: "0 0 0.5rem 0", fontStyle: "italic" }}>
+                                    {"Chief Bird Officer"}
+                                </p>
+                                <div style={{ display: "flex", alignItems: "center", gap: "0.5rem" }}>
+                                    <a
+                                        href="https://x.com/dry_tortuga"
+                                        target="_blank"
+                                        rel="noopener noreferrer nofollow">
+                                        <b>{"X"}</b>
+                                    </a>
+                                    <span>|</span>
+                                    <a
+                                        href="https://warpcast.com/dry-tortuga"
+                                        target="_blank"
+                                        rel="noopener noreferrer nofollow">
+                                        <b>{"Farcaster"}</b>
+                                    </a>
+                                </div>
+                            </div>
+                            <div
+                            	style={{
+                             		display: "flex",
+                               		flexDirection: "column",
+									alignItems: "center",
+									marginBottom: "1.5rem"
+                             	}}>
+                                <img
+                                    src="https://songbirdz.cc/images/1077.jpg"
+                                    className="rounded mb-2"
+                                    title="King Eider (1 of 1)"
+                                    style={{ width: "150px", height: "150px", objectFit: "cover" }} />
+                                <h3 style={{ margin: "0.25rem 0" }}>
+                                	{"Paesan"}
+                                </h3>
+                                <p style={{ margin: "0 0 0.5rem 0", fontStyle: "italic" }}>
+                                 {"Community Spotter"}
+                                </p>
+                                <div style={{ display: "flex", alignItems: "center" }}>
+                                    <a
+                                        href="https://x.com/paesan5"
+                                        target="_blank"
+                                        rel="noopener noreferrer nofollow">
+                                        <b>{"X"}</b>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
                     </Col>
                 </Row>
             </Container>
