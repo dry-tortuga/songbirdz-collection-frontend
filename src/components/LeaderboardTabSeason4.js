23722318c6f01af423a2ca9d335c485e5fb8c8c2
dry-tortuga <108436943+dry-tortuga@users.noreply.github.com>
2025-04-24 11:10:59 -0400

    update timestamp for points backfill

--- src/components/LeaderboardTabSeason4.js
+++ src/components/LeaderboardTabSeason4.js
@@ -60,7 +60,7 @@ const LeaderboardTabSeason4 = ({ onUserClick }) => {
                     <Row>
                         <Col>
 							<p>
-								{"Runs from March 1st, 2025 to May 31st, 2025 (11PM UTC). Points will attempt to auto-update in real-time, but will also be manually confirmed on a bi-weekly basis. Last confirmed on April 10th, 2025 (7PM UTC)."}
+								{"Runs from March 1st, 2025 to May 31st, 2025 (11PM UTC). Points will attempt to auto-update in real-time, but will also be manually confirmed on a bi-weekly basis. Last confirmed on April 24th, 2025 (3PM UTC)."}
 							</p>
                             <p>
                                 {"Accounts with the most Birder Points at the end of Season 4 will receive:"}
