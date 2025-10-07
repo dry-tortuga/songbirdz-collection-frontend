6a3db73cba3f91cfe6ccc2a87044d3237dc77940
dry-tortuga <108436943+dry-tortuga@users.noreply.github.com>
2025-10-07 19:50:58 -0400

    add contributors for the mp3 audio files

--- src/components/BirdAudioFile.js
+++ src/components/BirdAudioFile.js
@@ -2,15 +2,22 @@ import React from "react";
 
 const BirdAudioFile = (props) => {
 
-	const { birdId, ...restProps } = props;
+	const { bird, ...restProps } = props;
 
 	return (
-		<audio
-			{...restProps}
-			src={`${process.env.PUBLIC_URL}/audio/${birdId}.mp3`}
-			controlsList="nodownload"
-			controls
-			loop />
+		<div {...restProps}>
+			<audio
+				src={`${process.env.PUBLIC_URL}/audio/${bird.id}.mp3`}
+				style={{ width: '100%' }}
+				controlsList="nodownload"
+				controls
+				loop />
+			<p
+				className="text-muted text-center mt-1"
+				style={{ fontSize: '0.85rem' }}>
+				{bird.owner ? bird.species : ''} © {bird.audio_contributor_name}; Cornell Lab of Ornithology | Macaulay Library
+			</p>
+		</div>
 	);
 
 };
